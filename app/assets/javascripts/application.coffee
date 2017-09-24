3128a119c344b6bdbf4793b4075a1b05881f670d
Shawn Jung <shawn.jung@me.com>
2017-09-23 18:54:07 -0700

    added time filter

--- app/assets/javascripts/application.coffee
+++ app/assets/javascripts/application.coffee
@@ -1,3 +1,5 @@
 #= require jquery3
 #= require underscore
+#= require moment
+#= require moment-timezone
 #= require_tree ./plugins/jquery
--- app/controllers/api/riders/trips_controller.rb
+++ app/controllers/api/riders/trips_controller.rb
@@ -1,6 +1,6 @@
 class Api::Riders::TripsController < Api::RidersController
   def search
-    from_points = Trip::SpotPoint.near search_params[:from].values, (search_params[:radius]||0.5).to_f, unit: :km
+    from_points = Trip::SpotPoint.where('start_at >= ? and start_at <= ?', search_params[:start_at], search_params[:start_at] + 1.hour).near search_params[:from].values, (search_params[:radius]||0.5).to_f, unit: :km
     possible_point_ids = from_points.map(&:next_spot_point_ids).flatten.uniq
     to_points = Trip::SpotPoint.where(id: possible_point_ids).near(search_params[:to].values, (search_params[:radius]||0.5).to_f, unit: :km)
 
@@ -13,7 +13,10 @@ private
 
   def search_params
     return @_search_params if @_search_params.present?
-    @_search_params = params.permit(:time, :radius, from: [:lat, :lng], to: [:lat, :lng])
+    @_search_params = params.permit(:start_at, :radius, from: [:lat, :lng], to: [:lat, :lng])
+    @_search_params.tap do |whitelisted|
+      whitelisted[:start_at] = Time.parse whitelisted[:start_at]
+    end
     @_search_params
   end
 end
